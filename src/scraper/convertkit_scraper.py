68a3f7ec27c69d86d472ddeee02278fde0429123
Shane <shane@weareleanmean.com>
2025-01-03 17:45:20 -0800

    Updated Chrome configuration for Heroku

--- src/scraper/convertkit_scraper.py
+++ src/scraper/convertkit_scraper.py
@@ -39,17 +39,19 @@ class ConvertKitScraper:
         chrome_options.add_argument(f'user-data-dir={self.profile_dir}')
         chrome_options.add_argument('--profile-directory=Default')
         
-        # Add headless mode options if requested
-        if headless:
-            chrome_options.add_argument('--headless=new')  # Use new headless mode
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            
+        # Force headless mode and add Heroku-specific options
+        chrome_options.add_argument('--headless=new')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-software-rasterizer')
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_argument('--single-process')
+        chrome_options.add_argument('--remote-debugging-port=9222')
+        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
+        
         # Add stability options
         chrome_options.add_argument('--window-size=1920,1080')
-        chrome_options.add_argument('--remote-debugging-port=9222')
-        chrome_options.add_argument('--disable-extensions')
         chrome_options.add_argument('--disable-popup-blocking')
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
@@ -57,7 +59,7 @@ class ConvertKitScraper:
         
         try:
             print("\nInitializing Chrome driver...")
-            print(f"Headless mode: {'enabled' if headless else 'disabled'}")
+            print("Headless mode: enabled")
             self.driver = webdriver.Chrome(options=chrome_options)
             print("Chrome driver initialized successfully")
             
